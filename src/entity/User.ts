9280cbff7db4cf1216d2699db31ed2ce1564cc2a
Joseph Mwanzia <makamwanzia@gmail.com>
2024-11-05 23:19:00 +0300

    modified TypeORM, id now allows INT and BIGINT

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -2,8 +2,11 @@ import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
 
 @Entity({ name: 'app_user'})
 export class User {
-  @PrimaryGeneratedColumn()
-  id!: number;
+  // @PrimaryGeneratedColumn()
+  // id!: number;
+
+  @PrimaryGeneratedColumn({ type: "bigint" }) // Changing to BIGINT for future compatibility
+  id!: string; // TypeORM will handle bigint as string in JavaScript
 
   @Column()
   name!: string;
